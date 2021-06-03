b50fe66d1485cc3aa4afecba7ae76a6fea4b0e6c
Catherine <2651279837@qq.com>
2021-06-04 00:04:16 +0800

    修改了模糊知识

--- fuzzyKnowledge/fuzyy_init.py
+++ fuzzyKnowledge/fuzyy_init.py
@@ -4,22 +4,21 @@ import config
 
 
 #a b-----1：很少 2:较少 3:少  4：正常 5：多 6：较多 7：很多
-# c------1：不变 2:增加很小 3:增加较小 4:增加小   5：增加大 6：增加较大 7：增加很大
-#               8:减少很小 9:减少较小 10:减少小  11：减少大 12：减少较大 13：减少很大
+# c------8：不变 9:很小 10:较小 11:小   12：大 13：较大 14：很大
 
-C=[[1,2,3,4,5,6,7],
-   [8,1,2,3,4,5,6],
-   [9,8,1,2,3,4,5],
-   [10,9,8,1,2,3,4],
-   [11,10,9,8,1,2,3],
-   [12,11,10,9,8,1,2],
-   [13,12,11,10,9,8,1]
+C=[[8,9,10,11,12,13,14],
+   [9,8,9,10,11,12,13],
+   [10,9,8,9,10,11,12],
+   [11,10,9,8,9,10,11],
+   [12,11,10,9,8,9,10],
+   [13,12,11,10,9,8,9],
+   [14,13,12,11,10,9,8]
    ]
 cursor=config.connect.cursor()
-fuzzyMatrixId=1 #模糊矩阵id
+fuzzyMatrixId=8 #模糊矩阵id
 
-for precondition_a in range(1,8): #模糊集Aid
-    for precondition_b in range(1,8): #模糊集Bid
+for precondition_b in range(1,8): #模糊集Aid
+    for precondition_a in range(1,8): #模糊集Bid
         conclusion_c=C[precondition_a-1][precondition_b-1]  #模糊矩阵Cid
         pre_probablity_value=0.8 #前提可信度
         con_probablity_value=0.9 #结论可信度
