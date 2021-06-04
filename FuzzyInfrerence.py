c57d615d06799c0c36ba23acd6343bfc5fe19856
Catherine <2651279837@qq.com>
2021-06-04 16:05:17 +0800

    添加模糊解释器

--- FuzzyInfrerence.py
+++ FuzzyInfrerence.py
@@ -2,6 +2,13 @@ import getDataFromDB
 from fuzzyKnowledge import fuzzy_matrix
 import numpy as np
 
+#解释器，记录推理过程
+fuzzy_train=[]
+#车辆数据  1：很少 2:较少 3:少  4：正常 5：多 6：较多 7：很多
+# 时间变化幅度  8：不变 9:很小 10:较小 11:小   12：大 13：较大 14：很大
+car_time=['','很少','较少','少','正常','多','较多','很多',
+          '不变','很小','较小','小','大','较大','很大']
+
 # 对车流量进行模糊化处理
 def FuzzyVehicalCount(x):
     # 按照三角模糊法，取sigma为7
@@ -17,7 +24,7 @@ def FuzzyVehicalCount(x):
             result.append(temp)
         else:
             result.append(0)
-    print(result)
+    # print(result)
 
     return result
 
@@ -39,7 +46,7 @@ def huoji(num1 , num2):
 def getCount(type):
     VehicleData = getDataFromDB.getVehicleData()
     count = 0 #上一轮车辆通过数
-    Count2 = 0  #这轮车辆通过数
+    Count2 = 0  #上上轮车辆通过数
     gap = 0
     for i in range(len(VehicleData)):
         if(VehicleData[i][0] == type):
@@ -84,11 +91,11 @@ def SearchKnowledge():
 
     FuzzyKnowledge = getDataFromDB.getFuzzyKnowledgeData()
     Carnum = fuzzy_matrix.ReturnCarnum()
-    count1 , count2 = getCount(4)
+    count1 , count2 = getCount(1)
     fuzzycar1 = FuzzyVehicalCount(count1)
     fuzzycar2 = FuzzyVehicalCount(count2)
     Evidence = CaculateCompose(fuzzycar1,fuzzycar2)
-    print('evidence' , Evidence)
+    # print('evidence' , Evidence)
 
     sims = []
     for i in range(len(FuzzyKnowledge)):
@@ -103,12 +110,32 @@ def SearchKnowledge():
         sims.append(sim)
 
     kslice = sims.index(max(sims))
+    fuzzy_train.append('南北方向，使用模糊推理机 本轮车辆数：' + str(count1)+',上轮车辆数：' + str(count2))
+    fuzzy_train.append('使用三角法对车辆数据进行模糊化：\n'+str(fuzzycar1)+'\n'+str(fuzzycar2))
+    fuzzy_train.append('求出的最大匹配度为' + str(max(sims)))
+    fuzzy_train.append('计算出合成证据：'+ str(Evidence))
+    if (count1 == count2):
+        fuzzy_train.append('使用第'+str(kslice)+'条知识：' + 'IF x is 当前车辆(x1) '\
+                       +str(car_time[FuzzyKnowledge[kslice][0]])+' &上次车辆(x2)'
+                        +str(car_time[FuzzyKnowledge[kslice][1]])+' THEN y is 绿灯时间不变')
+    elif (count1 < count2):
+        fuzzy_train.append('使用第' + str(kslice) + '条知识：' + 'IF x is 当前车辆(x1) ' \
+                           + str(car_time[FuzzyKnowledge[kslice][0]]) + ' &上次车辆(x2)'
+                           + str(car_time[FuzzyKnowledge[kslice][1]]) + ' THEN y is 绿灯时间减少'
+                           +str(car_time[FuzzyKnowledge[kslice][2]]))
+    elif (count1 > count2):
+        fuzzy_train.append('使用第' + str(kslice) + '条知识：' + 'IF x is 当前车辆(x1) ' \
+                           + str(car_time[FuzzyKnowledge[kslice][0]]) + ' &上次车辆(x2)'
+                           + str(car_time[FuzzyKnowledge[kslice][1]]) + ' THEN y is 绿灯时间增加'
+                           + str(car_time[FuzzyKnowledge[kslice][2]]))
     return Evidence,FuzzyKnowledge[kslice][0],FuzzyKnowledge[kslice][1],FuzzyKnowledge[kslice][2]
 
 # 计算结论，前提与模糊矩阵相乘
 def CaculateConclusion():
     evidence, index1 ,index2 ,index3 = SearchKnowledge()
-    matrix = fuzzy_matrix.make_fuzzy_matrix(index1, index2 ,index3)
+    matrix = fuzzy_matrix.make_fuzzy_matrix(index1-1, index2-1 ,index3-8)
+    # print(index1-1 , index2-1 ,index3-8)
+
 
     matrix = np.array(matrix)
     temp = []
@@ -121,28 +148,43 @@ def CaculateConclusion():
     conclusion = conclusion.tolist()
 
     conclusion = fuzzy_matrix.ReturnLightTime(index3 - 8)
-
+    fuzzy_train.append('计算出结论：时间变化幅度为 '+str(conclusion))
+    # print('conclusion',conclusion)
     return conclusion
+
 
 # 结论去模糊化
 def Defuzzification():
 
     conclusion = CaculateConclusion()
-    print(conclusion)
+    # print(conclusion)
     maxvalue = max(conclusion)
     indexs = []
     for i in range(len(conclusion)):
         if(conclusion[i] == maxvalue):
             indexs.append(i)
-
-    return indexs[0]
+    fuzzy_train.append('使用最大隶属度法对结论进行去模糊化，时间变化： '+str(indexs[0])+'秒')
+    return indexs[0],fuzzy_train
 
 
 
 if __name__ == '__main__':
-    conclusion = Defuzzification()
 
-    print(conclusion)
+    count1 , count2 = getCount(1)
+    print(count1, count2)
+    conclusion = Defuzzification()
+    if(count1 == count2 or conclusion==0):
+        print('不变')
+    elif(count1 < count2):
+        print('绿灯时长减少{}秒'.format(conclusion))
+    elif (count1 > count2):
+        # conclusion = Defuzzification()
+        print('绿灯时长增加{}秒'.format(conclusion))
+
+    # print(fuzzy_train)
+    for th in fuzzy_train:
+        print(th)
+    # CaculateConclusion()
 
 
 
