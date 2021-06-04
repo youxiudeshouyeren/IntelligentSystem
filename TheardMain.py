52399ca201b14131063ff27bec4e8ff2263e4a70
Catherine <2651279837@qq.com>
2021-06-04 14:13:29 +0800

    添加模糊知识控制

--- TheardMain.py
+++ TheardMain.py
@@ -2,9 +2,10 @@ import sys
 sys.path.append('./LowerMachine')
 sys.path.append('./Vehicle')
 
-import UpperMachine
-import VehicleData_add
+from LowerMachine import UpperMachine
+from Vehicle import VehicleData_add
 import TrustInfernce
+import FuzzyInfrerence
 import time
 import datetime
 
@@ -29,22 +30,37 @@ def TheardMain():
             counts=my_upper_machine.getCount(i)
             beginTime,endTime=my_upper_machine.getBETime(i)
             VehicleData_add.vehicleData_add(i+1,counts,beginTime,endTime)
+            #南北34用模糊，东西12用可信度
+            if i in [1,2]:
+                # 用可信度推理机进行推理
+                conclusion = TrustInfernce.getConclusion(i+1)
+                print(conclusion)
+                # 根据结论对传感器进行修改
+                # 暂时只用可信度结论进行修改
+                if (conclusion[0] == "本轮绿灯时间不变"):
+                    # do Nothing
+                    print("")
+                elif (conclusion[0] == "本轮绿灯时间增加"):
+                    wait_time += wait_time * conclusion[1] * 4
+                elif conclusion[0] == "本轮绿灯时间减少":
+                    wait_time -= wait_time * conclusion[1]
+                else:
+                    print("Error!")
+            elif i in [3,4]:
+                # 用模糊推理机进行推理
+                count1, count2 = FuzzyInfrerence.getCount(i)
+                print(count1, count2)
+                if (count1 == count2):
+                    print('本轮绿灯时间不变')
+                elif (count1 < count2):
+                    conclusion = FuzzyInfrerence.Defuzzification()
+                    wait_time -=conclusion
+                    print('绿灯时长减少{}秒'.format(conclusion))
+                elif (count1 > count2):
+                    conclusion = FuzzyInfrerence.Defuzzification()
+                    wait_time += conclusion
+                    print('绿灯时长增加{}秒'.format(conclusion))
 
-            # 进行推理
-            conclusion = TrustInfernce.getConclusion(i+1)
-            print(conclusion)
-
-            # 根据结论对传感器进行修改
-            # 暂时只用可信度结论进行修改
-            if(conclusion[0]=="本轮绿灯时间不变"):
-                # do Nothing
-                print("")
-            elif (conclusion[0]=="本轮绿灯时间增加"):
-                wait_time+=wait_time*conclusion[1]*4
-            elif conclusion[0]=="本轮绿灯时间减少":
-                wait_time-=wait_time*conclusion[1]
-            else:
-                print("Error!")
             my_upper_machine.changeTime(i,wait_time)
             
 
